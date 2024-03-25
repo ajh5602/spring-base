9923bd6546b5c81b84eb6e30a43f6a1cf11c6b29
jaehyun <jaehyun@papamobility.com>
2024-03-25 14:07:53 +0900

    fix : deleted 기본 값 추가

--- src/main/java/com/taylor/project/base/entity/MemberGroup.java
+++ src/main/java/com/taylor/project/base/entity/MemberGroup.java
@@ -13,6 +13,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
@@ -48,6 +49,7 @@ public class MemberGroup {
     LocalDateTime updatedAt;
 
     @Column(name = "deleted_yn")
+    @ColumnDefault("'0'")
     Boolean deletedYn;
 
     @Builder
